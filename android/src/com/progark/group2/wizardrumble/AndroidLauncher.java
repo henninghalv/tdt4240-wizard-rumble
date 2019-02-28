40cec969afbf28dacaf1f592a6c6a78c5d012dc7
Henning Bang Halvorsen <henning.b.halvorsen@gmail.com>
2019-02-28 12:18:39 +0100

    Initial commit: Setup a default LibGDX project with KryoNet as a dependency.

---
+++ android/src/com/progark/group2/wizardrumble/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.progark.group2.wizardrumble;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.progark.group2.wizardrumble.Application;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Application(), config);
+	}
+}
---
+++ desktop/src/com/progark/group2/wizardrumble/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.progark.group2.wizardrumble.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.progark.group2.wizardrumble.Application;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Application(), config);
+	}
+}
