d447d0155792d21c9788b08a853e2cc950838069
stianbm <33124209+stianbm@users.noreply.github.com>
2019-03-18 13:39:49 +0100

    Added textbutton on top of button, added listener to stack

--- core/src/com/progark/group2/wizardrumble/states/MainMenuState.java
+++ core/src/com/progark/group2/wizardrumble/states/MainMenuState.java
@@ -2,16 +2,24 @@ package com.progark.group2.wizardrumble.states;
 
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.progark.group2.wizardrumble.Application;
+
+import java.awt.Font;
 
 /** The main menu that is shown when the game is launched
  */
@@ -22,6 +30,8 @@ public class MainMenuState extends State {
     private Stage stage;
     private Table table;
     private Viewport viewport;
+    private TextButton textButton;
+    private TextButton.TextButtonStyle textButtonStyle;
 
     public MainMenuState(GameStateManager gameStateManager){
         super(gameStateManager);
@@ -33,8 +43,32 @@ public class MainMenuState extends State {
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
 
+
+        BitmapFont font = new BitmapFont();
+        font.setColor(Color.BLACK);
+        textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = font;
+        textButton = new TextButton("Start", textButtonStyle);
+
+        Stack button = new Stack();
+        button.add(buttonImage);
+        button.add(textButton);
+
+        button.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("Hallo?");
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("Bye");
+            }
+        });
+
         table = new Table();
-        table.add(buttonImage).size(buttonImage.getWidth(), buttonImage.getHeight());
+        table.add(button);
         table.setFillParent(true);
 
         stage.addActor(table);
