27100578f3a925b4b75635d955acbad3f0d688c3
Henning Bang Halvorsen <henning.b.halvorsen@gmail.com>
2019-03-25 11:41:41 +0100

    WIP: #3: Updated logic for changing server and port statuses once they are in use. As of now, they are not reopened after use. Added a lot of print statements to follow the process on the server. These will ultimately be removed, or improved.

--- gameserver/src/main/java/com/progark/group2/gameserver/KryoServerRegister.java
+++ gameserver/src/main/java/com/progark/group2/gameserver/KryoServerRegister.java
@@ -1,10 +1,11 @@
-package com.progark.group2.gameserver.resources;
+package com.progark.group2.gameserver;
 
 import com.badlogic.gdx.math.Vector2;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.Server;
 import com.progark.group2.wizardrumble.network.requests.PlayerDeadRequest;
 import com.progark.group2.wizardrumble.network.requests.PlayerJoinedRequest;
+import com.progark.group2.wizardrumble.network.requests.PlayerNamesRequest;
 import com.progark.group2.wizardrumble.network.responses.PlayerJoinedResponse;
 import com.progark.group2.wizardrumble.network.requests.PlayerMovementRequest;
 import com.progark.group2.wizardrumble.network.requests.PlayerNameRequest;
@@ -12,8 +13,10 @@ import com.progark.group2.wizardrumble.network.responses.PlayerNameResponse;
 import com.progark.group2.wizardrumble.network.responses.PlayerStatisticsResponse;
 import com.progark.group2.wizardrumble.network.requests.PlayersHealthStatusRequest;
 import com.progark.group2.wizardrumble.network.responses.ServerErrorResponse;
+import com.progark.group2.wizardrumble.network.responses.ServerSuccessResponse;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class KryoServerRegister {
@@ -28,12 +31,15 @@ public class KryoServerRegister {
         kryo.register(PlayerJoinedRequest.class);
         kryo.register(PlayerJoinedResponse.class);
         kryo.register(PlayerMovementRequest.class);
+        kryo.register(PlayerNamesRequest.class);
         kryo.register(PlayerNameRequest.class);
         kryo.register(PlayerNameResponse.class);
         kryo.register(PlayersHealthStatusRequest.class);
         kryo.register(PlayerStatisticsResponse.class);
         kryo.register(ServerErrorResponse.class);
+        kryo.register(ServerSuccessResponse.class);
         kryo.register(HashMap.class);
         kryo.register(Vector2.class);
+        kryo.register(ArrayList.class);
     }
 }
